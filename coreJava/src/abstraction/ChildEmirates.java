775094f4bfd65e59f2a8979fcd4f8493e0641771
rhino0419 <rhino0419@gmail.com>
2020-12-27 20:05:13 +0530

    This tester13

--- coreJava/src/abstraction/ChildEmirates.java
+++ coreJava/src/abstraction/ChildEmirates.java
@@ -16,16 +16,15 @@ public class ChildEmirates extends ParentAirCraft {
 	public void bodyColor() {
 		// TODO Auto-generated method stub
 		System.out.println("Body color of emirates is red");
-<<<<<<< HEAD
-		 
-||||||| c397a6d
-=======
+ 
 		
 		System.out.println("Hi this is git test1");
 		System.out.println("Hi this is git test2");
 		System.out.println("Hi this is git test3");
 		System.out.println("Hi this is git test4");
->>>>>>> 42a677bc1fbfd772617519fb8528f0101d2eae62
+		System.out.println("India");
+ 
+
 	}
 
 }
